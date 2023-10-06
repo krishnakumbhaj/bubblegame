5586fa00b76604bcea1efdf107f374a87e0a6176
krishnakumbhaj <144392084+krishnakumbhaj@users.noreply.github.com>
2023-10-06 19:33:23 +0530

    Add files via upload

---
+++ 8bubble_game.js
@@ -0,0 +1,65 @@
+
+
+
+var score=0 
+var hit
+
+function makebubble(){
+            var clutter =""
+            for( var i = 1 ; i<=168  ; i++)
+            {      
+                       var num= Math.floor(Math.random()*10)
+                        clutter+=` <div class="bubble">${num}</div>`
+                      
+            }
+            document.querySelector(".panel2").innerHTML = clutter
+}
+makebubble()
+
+function hitnumber(){
+          hit = Math.floor(Math.random()*10)
+            document.querySelector("#hit").textContent = hit
+}
+hitnumber()
+var timer =60
+function runtimer(){
+          var timmerend =  setInterval(function(){
+                        if(timer>0)
+                        {
+                        timer--
+                        document.querySelector("#timer").textContent = timer
+
+                        }
+                        else{
+                                    clearInterval(timmerend)
+                                    document.querySelector(".panel2").innerHTML =`<h2 class ="h" >Game Over</h2>`
+                        }
+            },1000)
+}
+runtimer()
+
+function increasescore(){
+                        score+=10
+                        document.querySelector("#score").textContent = score
+}
+function increasescorenot(){
+                        score-=5
+                        document.querySelector("#score").textContent = score
+}
+
+document.querySelector(".panel2").addEventListener("click",function(dets ){
+
+          var match =Number(dets.target.textContent) 
+            if(match ==  hit)
+            {
+                        increasescore();
+                        makebubble();
+                        hitnumber();
+            }
+            else
+            {
+                        increasescorenot();
+                        makebubble();
+            }
+})
+
